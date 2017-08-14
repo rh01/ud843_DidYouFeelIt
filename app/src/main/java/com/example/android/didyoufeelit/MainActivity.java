19493d7eb36860d38e326bd45bb2ce0220453c09
rh01 <1048157315@qq.com>
2017-08-14 15:01:24 +0800

    fix error.

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.UpdateAppearance;
 import android.widget.TextView;
+
+import static com.example.android.didyoufeelit.Utils.fetchEarthquakeData;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -34,11 +38,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        /*// Perform the HTTP request for earthquake data and process the response.
+        Event earthquake = fetchEarthquakeData(USGS_REQUEST_URL);
 
         // Update the information displayed to the user.
-        updateUi(earthquake);
+        updateUi(earthquake);*/
+
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
     }
 
     /**
@@ -53,5 +61,42 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event>{
+        /**
+         * 此方法在后台线程上激活（调用），因此我们可以执行
+         * 诸如做出网络请求等长时间运行操作。
+         *
+         * 因为不能从后台线程更新 UI，所以我们仅返回
+         * {@link Event} 对象作为结果。
+         */
+        @Override
+        protected Event doInBackground(String... urls) {
+            // 如果不存在任何 URL 或第一个 URL 为空，切勿执行请求。
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            Event result = Utils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+        /**
+         * 此方法是在完成后台工作后，在主 UI 线程上
+         * 激活的。
+         *
+         * 可以在此方法内修改 UI。我们得到 {@link Event} 对象
+         * （该对象从 doInBackground() 方法返回），并更新屏幕上的视图。
+         */
+        @Override
+        protected void onPostExecute(Event result) {
+            // 如果不存在任何结果，则不执行任何操作。
+            if (result == null) {
+                return;
+            }
+
+            /*super.onPostExecute(event);*/
+            updateUi(result);
+        }
     }
 }
